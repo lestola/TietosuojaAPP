5b7954fb2693d06086497e9e0f4c14b9a017c512
marko.lehtola <marko.lehtola@edu.xamk.fi>
2017-06-01 09:55:44 +0300

    * kysymykset.json: muutettu luokat peliin sopiviksi
    * Question.cs:
    * Ohje.xaml.cs:

--- TietosuojaApp3/Ohje.xaml.cs
+++ TietosuojaApp3/Ohje.xaml.cs
@@ -11,7 +11,8 @@ namespace TietosuojaApp3
 
     public partial class Ohje : ContentPage
     {
-        
+
+        public static string jsonData;
 
         public Ohje()
         {
@@ -22,35 +23,29 @@ namespace TietosuojaApp3
         void json_Serialized_Clicked(object sender, System.EventArgs e)
         {
             
-			Person person1 = new Person("Fred", 5);
-			Person person2 = new Person("Molly", 13);
+			Question person1 = new Question("Kuka on Suomen presidentti?", 3,"Kekkonen","Ahtisaari","Niinistö","Mannerheim");
+			Question person2 = new Question("Mikä on seuraavista arvokkain metalli?", 2, "Hopea","Platina","Kulta","Kupari");
 			object[] t = new object[2];
 			t[0] = person1;
 			t[1] = person2;
 
-            string jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(t);
+            jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(t);
             //string json = JsonWriter.Serialize(t);
             // gives me the right string - [{"Name":"Fred","Age":5},{"Name":"Molly","Age":13}]
             txtJSON.Text = jsonData;
-
-            Person[] jsonObject = Newtonsoft.Json.JsonConvert.DeserializeObject<Person[]>(jsonData);
-
-			//foreach (var row in jsonObject)
-            //    txtVastaus.Text = row.Name;
-
-            txtQuestion.Text = jsonObject.ElementAt(0).Name;
-            txtVastaus.Text = jsonObject.ElementAt(1).Name;
-
-                
-
-
 		
 		}
 
 
         void json_DeSerialized_Clicked(object sender, System.EventArgs e)
-        { 
-            
+        {
+			Question[] jsonObject = Newtonsoft.Json.JsonConvert.DeserializeObject<Question[]>(jsonData);
+
+			//foreach (var row in jsonObject)
+			//    txtVastaus.Text = row.Name;
+
+			txtQuestion.Text = jsonObject.ElementAt(0).QuestionToAsk;
+			txtVastaus.Text = jsonObject.ElementAt(1).QuestionToAsk;
         }
 
     }
--- TietosuojaApp3/Question.cs
+++ TietosuojaApp3/Question.cs
@@ -2,20 +2,28 @@
 
 namespace TietosuojaApp3
 {
-	public class Person
+	public class Question
 	{
-		public string Name { get; set; }
-		public int Age { get; set; }
+		public string QuestionToAsk { get; set; }
+		public int CorrectAnswer { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
 
-		public Person()
+		public Question()
 		{
 			// this is the default constructor that JSONFX wanted.
 		}
 
-		public Person(string name, int age)
+		public Question(string questionToAsk, int correctAnswer, string answer1, string answer2, string answer3, string answer4)
 		{
-			Name = name;
-			Age = age;
+			QuestionToAsk = questionToAsk;
+			CorrectAnswer = correctAnswer;
+            Answer1 = answer1;
+            Answer2 = answer2;
+            Answer3 = answer3;
+            Answer4 = answer4;
 		}
 	}
 
